fddececa552704f125d1b69fbc440f462c97ef7e
steelworx <eileencuevas62@gmail.com>
2019-01-08 17:45:39 -0500

    created PUT and DELETE endpoints in index.js

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ server.post('/api/posts', (req, res) => {
     if (postData.title.length >= 1 && postData.contents.length >= 1){
         db
             .insert(postData)
-            .then(newPost => {
-                res.status(201).json(newPost);
+            .then(newPostId => {
+                res.status(201).json({ ...postData, ...newPostId });
             })
             .catch(() => {
                 res.status(500).json({ "error": 'There was an error while saving the post to the database' });
@@ -53,6 +53,59 @@ server.post('/api/posts', (req, res) => {
     } else {
         res.status(400).json({ "errorMessage": 'Please provide title and contents for the post.' })
     }
+})
+
+server.put('/api/posts/:id', (req, res) =>  {
+    const id = req.params.id;
+    const dataForUpdating = req.body;
+
+    db
+        .findById(id)
+        .then(post => {
+            if (post.length > 0) {
+                if (dataForUpdating.title && dataForUpdating.contents) {
+                    db
+                        .update(id, dataForUpdating)
+                        .then(() => {
+                            res.status(200).json({ id, ...dataForUpdating });
+                        })
+                        .catch(() => {
+                            res.status(500).json({ "error": 'The post information could not be modified.' });
+                        })
+                } else {
+                    res.status(400).json({ "errorMessage": 'Please provide title and contents for the post.' });
+                }
+            } else {
+                res.status(404).json({ "message": 'The post with the specified ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ "error": 'The posts information could not be retrieved.' })
+        })
+})
+
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+        .findById(id)
+        .then(post => {
+            if (post.length > 0) {
+                db
+                    .remove(id)
+                    .then(() => {
+                        res.status(200).json(post)
+                    })
+                    .catch(() => {
+                        res.status(500).json({ "message": 'The post could not be removed.' })
+                    })
+            } else {
+                res.status(404).json({ "message": 'The post with the specified ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ "error": 'The posts information could not be retrieved.' })
+        })
 })
 
 server.listen(5000, () => console.log('Server Running...'))
