7107297b367160a93230260b4f2bc3e4b9235c86
steelworx <eileencuevas62@gmail.com>
2019-01-08 15:53:09 -0500

    added POST endpoints using index.js

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.use(express.json());
 server.use(cors());
 
 server.get('/api/posts/', (req, res) => {
-        db
-            .find()
-            .then(posts => {
-                res.status(200).json(posts)
-            })
-            .catch(() => {
-                res.status(500).json({ 'error': `'The posts information could not be retrieved.'` })
-            });
+    db
+        .find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(() => {
+            res.status(500).json({ "error": 'The posts information could not be retrieved.' })
+        });
     })
 
 server.get('/api/posts/:id', (req, res) => {
@@ -29,12 +30,29 @@ server.get('/api/posts/:id', (req, res) => {
         .then(post => {
             {post.length > 0 ? 
                 res.status(200).json(post) : 
-                res.status(404).json({ 'error': 'The post information could not be retrieved.' })
+                res.status(404).json({ "error": 'The post information could not be retrieved.' })
             }
         })
         .catch(() => {
-            res.status(500).json({ 'error': 'The posts information could not be retrieved.' })
+            res.status(500).json({ "error": 'The posts information could not be retrieved.' })
         });
+})
+
+server.post('/api/posts', (req, res) => {
+    const postData = req.body;
+    
+    if (postData.title.length >= 1 && postData.contents.length >= 1){
+        db
+            .insert(postData)
+            .then(newPost => {
+                res.status(201).json(newPost);
+            })
+            .catch(() => {
+                res.status(500).json({ "error": 'There was an error while saving the post to the database' });
+            });
+    } else {
+        res.status(400).json({ "errorMessage": 'Please provide title and contents for the post.' })
+    }
 })
 
 server.listen(5000, () => console.log('Server Running...'))
